9538f117f450547f0a43650f75fe99afba23827e
carlo-varri <121613137+carlo-varri@users.noreply.github.com>
2023-01-29 01:13:19 +0000

    Making comments more clear, finshing the clean which included; renaming columns, editing the strings in one of my columns, filling in missing numerical NA's by region median, and pivoting data.
    Then moved onto data visualisation, plotting immigration trend as a total, and then visualising the trend by both development status and continent.

--- Canada_immigration.R
+++ Canada_immigration.R
@@ -14,58 +14,79 @@ library(janitor)
 
 ###IMPORTING ####
 
-# a function to import the four excel files in my wd
+# a function to list .xlsx files in my wd, and then import them
+#I came back to this and added .name_repair, as reading in the columns 
+#with date names was causing forcing backticks into col names 
 file_list <- list.files(pattern='*.xlsx')
-df_list <- lapply(file_list, read_excel, skip = 1) #reading in the files, skipping the first row  
-#now I want to delete the header name for df's 2 3 and 4 so I can bind rows 
-names(df_list[c(2,3,4)]) <- NULL #I dont think this was necessary
-#binding rows to create one large dataset
+df_list <- lapply(file_list, read_excel, skip = 1, 
+                  .name_repair = ~ make.names(., unique = TRUE))
+
+# next I am binding rows to create one large dataset
 df1 <- bind_rows(df_list)
 
 ### CLEANING ####
 
-names(df1) <- tolower(names(df1)) #all col names to lower 
-sapply(df1[1:2], unique) #can see first two cols are uninteresting so will drop them 
+#all col names to lower 
+names(df1) <- tolower(names(df1))
+
+#can see first two cols are uninteresting so will drop them 
 df1 <- select(df1,-c(1,2))
+
 #area, reg and dev also have no use to me 
 df1 <- select(df1,-c(area, reg, dev))
+
 #now, I want to remove the rows which are all NA.
 #let me see if there are any columns in which the only NA's are those which are NA rows 
 vis_miss(df1)
-#second row, areaname is the winner. 
+
+#second row, areaname is the winner. The only NAs full row NAs
 #so, filtering out rows if they have NA in col. 2
 df1 <- df1 %>% filter(!is.na(areaname)) 
+
 #vis_miss(df) shows this has worked
-#also removing row of zeros
-df2 <- df1 %>% filter(!areaname == 0) #new df 
+#also removing row of zeros and assigning this to a new dataset 
+df2 <- df1 %>% filter(!areaname == 0) 
+
 #now, I want to set the areaname and regname to factors
 #first, let me the area/ region names are as expected ... 
 sapply(df2[c("areaname", "regname")], unique)
+
 #okay, I can see we have some typos in both
-#luckily, I have data on the correct spellings for both 
+#luckily, I have data on the correct spellings for both.
+#importing this in 
 spellings <- read_excel("~/Desktop/Learning R/Canada/Correct_spellings.xlsx")
+
 #pulling the unique areanames from spelling
 areas <- as.tibble(unique(spellings$AreaName))
 names(areas) <- "AreaName"
+
 #creating special use tibble for this 
 df_areas <- as.tibble(df2$areaname)
 names(df_areas) <- "AreaName"
 
 #comparing area entries in the df with correct spellings 
 correct_areas <- stringdist_left_join(df_areas, areas, by = "AreaName", method ="qgram")
-#introduced 2 NAs - replacing manually 
+
+#introduced 2 NAs, btoh of which relate to the same area, 
+#so I will replace these manually 
 correct_areas <- replace(correct_areas, is.na(correct_areas), "Latin America and the Caribbean")
+
 #replacing areaname column in df with the corrected names 
 df2['areaname'] <- correct_areas['AreaName.y']
 
-#now fixing typose for country names 
-#but this time using reclin with a blocking var of area names  
-#hopefully this avoids NAs
-
+#now, I want to fix the typose in country names. However, I want to do this 
+#in a neater way than above, as creating and then manually replacing NAs
+#is arduous
+#so, this timeI'll use reclin with a blocking var of area names. 
+#first, creating a dataset with correct countries, and pulling out the 
+#current countries from df2. Notice I've also included areanames in the 
+#new dataframes. This is because I will use these as a blocking var. 
 countries <- spellings[c("OdName", "AreaName")]
 df_countries <- df2[c('odname', 'areaname')]
 names(countries) <- tolower(names(countries))
 
+#now creating a dataset with the typo'd countries matched to the correct 
+#ones. 
 a <- pair_blocking(countries, df_countries, 
               blocking_var = "areaname"
               ) %>% 
@@ -74,8 +95,10 @@ a <- pair_blocking(countries, df_countries,
   link()
 
 
-#an aside to test if reclin worked or they just lined up, as 
-#they were in the same order before 
+#This worked, but I was unsure if that was only because the correct 
+#spelling and the typos were all in the same order to start with. 
+#so, I ran it again with the typos in a random order 
+#Note, this is where I first discovered the backquote issue
 
 #x <- df %>% arrange(desc(`1980`))
 #testing <-x[c('odname', 'areaname')]
@@ -87,12 +110,16 @@ a <- pair_blocking(countries, df_countries,
 #select_greedy(weight = "odname") %>% 
 #link()
 #Yes, it did actually work. See order of rows in y is as expected 
-#so, ready to put the corrected column into df
+#so, now I have comfort this is a good method for future, 
+#I'm ready to put the corrected column names back into df2
 
 df2['odname'] <- a['odname.x'] 
 
+#mathcing typos and correct spellings using reclin took me a long time 
+#to get right. 
+#Below I document previous failed attempts at correcting typos/ NA's,
 
-#documenting previous failed attempts at correcting typos/ NA's:
+#••••••••••••••••••••••••••••••
 
 #countries_na <- correct_countries %>% 
 #  filter(is.na(value.y)) %>% 
@@ -108,8 +135,6 @@ df2['odname'] <- a['odname.x']
   #select(value.x) %>% 
  # head(4)
 #these are my four NA's corrected. Now I need to get these into corrcet_countries
-
-
 
 #After original str_dist_left_join
 
@@ -136,38 +161,146 @@ df2['odname'] <- a['odname.x']
 #this didnt work as compare_pairs had introcued new rows without me realising
 #so, there were more rows that could fit into df
 
+#••••••••••••••••••••••••••••••
+
 #anyway... back to the cleaning.... 
 
 #chekcing if regname is okay... 
 #unique(df$regname) - can see some typos... 
 #so, doing the same for this col, using the fact that we have 
-#areaname to cross compare... (also have same od name but thats too easy)
+#areaname to cross compare (note, I know I also have all the country names 
+#fixed for typos, so I could just use them to match, but thought that 
+#was a bit too easy)
 
 regions <- spellings[c('AreaName','RegName')]
 names(regions) <- tolower(names(regions))
 df_regions <- df2[c('areaname','regname')]
 
+#now creating a dataset with the typo'd areas matched to the correct 
+#ones. 
 b<-pair_blocking(regions, df_regions, 
               blocking_var = "areaname") %>% 
   compare_pairs(by ="regname", default_comparator = lcs()) %>% 
   select_greedy(weight = "regname") %>% 
   link()
 
-#successful. Now putting correct 
+#successful. Now putting into my original dataframe 
 df2['regname'] <- b['regname.x'] 
+
 #okay, so first three rows are taken care of... finally. 
 #checking if the fourth is okay 
 #unique(df$devname)
-#all good... thankgod I didnt mess these ones up too 
+#yep, all good.
 
 #now I can begin to do some more standard cleaning, 
 #starting with removing the annoying backticks from the year columns 
-df3<- clean_names(df2) #new df 
+#df3<- clean_names(df2)
 #using janitor package to remove backticks... worked but added x in front of 
 #all my year columns 
 #I dont know how to remove this... gsub just brings the ticks back 
 #i think the issue originates from data import... mustve been something 
 #amiss with the excel column names... will continue with the xcolumns for 
 #now...  
+#I ended up asking someone at work about these backtick. They told me 
+#this is because R cannot handle numerical col. names. I ended up
+#editing my reads_excel function above to which introduced 'X's'
+#as well. This is the best I can do. 
+#I'll now edit the col headers to read CY_xxxx, denoting calendar year
 
+colnames(df2) <- str_replace(colnames(df2),"x","CY_")
+
+#now, I want to rename odname to country, 
+#areaname to continent, regname to region and devname to development_status
+#then dropping the old column names
+df2<- df2 %>% mutate(country = odname, 
+                  continent = areaname, 
+                  region = regname,
+                  dev_status_of_region = devname,
+                .before= odname) %>% 
+                select(-c(odname,areaname,regname,devname))
+
+#now, I want to turn area, region and dev_status to factors,
+#and assign this to a new dataframe 
+
+cols_to_become_factors <- names(df2[1:4])
+df2[cols_to_become_factors] <- lapply(df2[cols_to_become_factors], factor)
+df3 <- df2
+
+#I think I'm ready to end the cleaning now. 
+#Just checking no NA's exist 
+vis_miss(df3)
+
+#Okay, so there are rows of NA data across all years for three countries 
+#I want to fill in each column with the median number from the same region,
+#in the same year
+
+df4 <- df3 %>% 
+  group_by(region) %>% 
+  mutate_at(vars(CY_1980:CY_2013), 
+            ~replace_na(., 
+                        median(., na.rm = TRUE))) 
+#I want to pivot my data so we have a date column, rather than 
+#a column per year, and makign it numeric 
+
+df4 <- df4%>% pivot_longer(cols=c(CY_1980:CY_2013),
+                    names_to='year',
+                    values_to='no_of_immigrants')
+df4$year <- gsub("CY_","",df4$year) %>% as.numeric()
+
+#Finally, looking at dataframe, I want to remove 'regions' from dev_status_of_regions
+df4$dev_status_of_region <- gsub("regions", "", df4$dev_status_of_region)
+
+#okay, cleaning come to an end 
+
+#### ANALYSIS AND VISUALISATION ####
+#examples of questions I want to answer:
+#1. What has the overall immigration trend been since 1980?
+#2. What has the trend in each area been since 1980?
+#3. What has the trend in each region been since 1980?
+#4. Which region has experienced highest % change in immigration since 1980?
+#5. Which country has experienced highest % change in immigration since 1980?
+#6. Has there been more migration from developed or developing countries since 1980?
+
+#I wont stick to these religiously. I'll start with the first, but will 
+#deviate if more interesting analysis opens up 
+
+#1. What has the overall immigration trend been since 1980?
+#I'll use ggplot to visualise the total since 1980
+
+plot_df_1 <- df4 %>% filter(country == 'Total')
+plot_1<- ggplot(Totals, aes(year, no_of_immigrants)) +
+  geom_line()
+
+#now, I want to see how developed vs devloping has looked in this time 
+#, as well as editing the labels 
+plot_df_2 <- df4 %>% group_by(dev_status_of_region,year) %>% 
+  summarise(total_per_dev_status = sum(no_of_immigrants))
+
+plot_2 <- ggplot(plot_df_2, aes(x = year, 
+                      y= total_per_dev_status, 
+                      group = dev_status_of_region,
+                      colour = dev_status_of_region))+ 
+  geom_line() +
+    labs(y = "No. of immigrants",
+         x = "Year") +
+  labs(color='Legend') 
+
+
+
+#Now the same, by continent, and removing the legend title 
+
+plot_df_3 <- df4 %>% 
+  filter(!continent == 'World')  %>% 
+  group_by(continent,year) %>% 
+  summarise(total_per_continent = sum(no_of_immigrants)) %>% 
+  ungroup()
+
+plot_3 <- ggplot(plot_df_3, aes(x = year, 
+                      y= total_per_continent, 
+                      group = continent,
+                      colour = continent))+ 
+  geom_line() + 
+  labs(y = "No. of immigrants",
+       x = "Year") +
+  scale_color_discrete(name="")
 
